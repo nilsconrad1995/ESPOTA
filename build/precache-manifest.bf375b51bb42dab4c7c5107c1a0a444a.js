9d10f731ebcb7abb2d4648849bbbfc20dcc3c021
EGI <nils.conrad@stihl.de>
2021-02-16 09:24:44 +0100

    Added Updater

---
+++ build/precache-manifest.bf375b51bb42dab4c7c5107c1a0a444a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fcb6ee91333b0d13a779f2b675479a3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dbe69dd3661ed1c2a53c",
+    "url": "./static/css/main.7e6de23a.chunk.css"
+  },
+  {
+    "revision": "63434a638cabb531d86c",
+    "url": "./static/js/2.ddd56c7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ddd56c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe69dd3661ed1c2a53c",
+    "url": "./static/js/main.2bce116f.chunk.js"
+  },
+  {
+    "revision": "dc8d468ff77d3058a45e",
+    "url": "./static/js/runtime-main.6b54ca97.js"
+  }
+]);
